06ce8ca80463b6a1a340489ccc615317b99dde9b
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2024-05-29 19:39:41 +0200

    chore: Publish cargo package

--- cli/build.rs
+++ cli/build.rs
@@ -1,12 +1,17 @@
-fn main() {
-    println!("cargo:rerun-if-changed=../shared/conf.h");
-    let bindings = bindgen::Builder::default()
-        .header("../shared/conf.h")
-        .clang_arg("-I../shared") // Specify the include path for additional headers if needed
-        .generate()
-        .expect("Unable to generate bindings");
+use std::path::Path;
 
-    bindings
-        .write_to_file("src/config/raw_bindings.rs")
-        .expect("Couldn't write bindings!");
+fn main() {
+    // if it is not keep using the last one
+    if Path::new("../shared/conf.h").exists() {
+        println!("cargo:rerun-if-changed=../shared/conf.h");
+        let bindings = bindgen::Builder::default()
+            .header("../shared/conf.h")
+            .clang_arg("-I../shared") // Specify the include path for additional headers if needed
+            .generate()
+            .expect("Unable to generate bindings");
+
+        bindings
+            .write_to_file("src/config/raw_bindings.rs")
+            .expect("Couldn't write bindings!");
+    }
 }
